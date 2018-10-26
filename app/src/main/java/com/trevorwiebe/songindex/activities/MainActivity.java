ac9b132022aed9af329890e935a7bd461713756a
trevor <thisistrevor4@gmail.com>
2018-10-26 17:07:33 -0500

    connect AddNewSongActivity to MainActivity, add firebase json file

--- app/src/main/java/com/trevorwiebe/songindex/activities/MainActivity.java
+++ app/src/main/java/com/trevorwiebe/songindex/activities/MainActivity.java
@@ -1,7 +1,11 @@
     package com.trevorwiebe.songindex.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.trevorwiebe.songindex.R;
 
@@ -12,4 +16,22 @@ import com.trevorwiebe.songindex.R;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.main_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+            if(item.getItemId() == R.id.add_new_song_menu){
+                Intent add_new_song_intent = new Intent(MainActivity.this, AddSongActivity.class);
+                startActivity(add_new_song_intent);
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
